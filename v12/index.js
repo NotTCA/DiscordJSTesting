3934e05a4d6cbcab09b0476a9e2054821fd4af62
Not TCA <theabcashow.josh@gmail.com>
2021-09-02 19:07:13 +0800

    was bored so I watched some WOK tutorials

--- v12/index.js
+++ v12/index.js
@@ -1,46 +1,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 const config = require("./config.json");
 require("dotenv").config();
 
-const command = require("./command");
-const firstMessage = require('./first-message')
+const command = require('./command')
 
 client.on("ready", () => {
   console.log(`${client.user.tag} is ready to be used!`);
 
-  command(client, "ping", (message) => {
-    message.channel.send("Pong!");
-  });
+  command(client, 'create-text-channel', message => {
+    const name = message.content.replace('!create-text-channel ', '')
 
-  command(client, "servers", (message) => {
-    client.guilds.cache.forEach((guild) => {
-      message.channel.send(
-        `${
-          guild.name
-        } has a total of ${guild.memberCount.toLocaleString()} members.`
-      );
-    });
-  });
+    message.guild.channels.create(name, {
+      type: 'text'
+    }).then(channel => {
+      const categoryID = '879167044241735710'
+      channel.setParent(categoryID)
+    })
+  })
+  
+  command(client, 'create-voice-channel', message => {
+    const name = message.content.replace('!create-voice-channel ', '')
 
-  command(client, ["cc", "clearchannel"], (message) => {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.delete();
-    message.channel.messages.fetch().then((results) => {
-      message.channel.bulkDelete(results);
-    });
-  });
-
-  command(client, "status", (message) => {
-    const status = message.content.replace("!status", "");
-    client.user.setPresence({
-      activity: {
-        name: status,
-        type: 0,
-      },
-    });
-  });
-
-  firstMessage(client, "882880394800824382", "hello world", ['🔥'])
+    message.guild.channels.create(name, {
+      type: 'voice'
+    }).then(channel => {
+      const categoryID = '879167044241735710'
+      channel.setParent(categoryID)
+      channel.setUserLimit(10)
+    })
+  })
 });
 
 client.login(process.env.TOKEN);
