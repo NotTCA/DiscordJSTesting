1db79b66aa35b1d53783149b60c4d051dd5b63a1
Not TCA <theabcashow.josh@gmail.com>
2021-09-05 15:00:48 +0800

    updated index.js on v12-wok

--- v12-wok/index.js
+++ v12-wok/index.js
@@ -4,33 +4,8 @@ const client = new Discord.Client();
 const config = require("./config.json");
 require("dotenv").config();
 
-const command = require('./command')
-
 client.on("ready", () => {
-  console.log(`${client.user.tag} is ready to be used!`);
-
-  command(client, 'create-text-channel', message => {
-    const name = message.content.replace('!create-text-channel ', '')
-
-    message.guild.channels.create(name, {
-      type: 'text'
-    }).then(channel => {
-      const categoryID = '879167044241735710'
-      channel.setParent(categoryID)
-    })
-  })
-  
-  command(client, 'create-voice-channel', message => {
-    const name = message.content.replace('!create-voice-channel ', '')
-
-    message.guild.channels.create(name, {
-      type: 'voice'
-    }).then(channel => {
-      const categoryID = '879167044241735710'
-      channel.setParent(categoryID)
-      channel.setUserLimit(10)
-    })
-  })
+  console.log(`The bot is ready!`);
 });
 
 client.login(process.env.TOKEN);
