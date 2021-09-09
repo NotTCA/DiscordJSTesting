da37e74be55072aef7f872296888316454101072
Not TCA <theabcashow.josh@gmail.com>
2021-09-09 10:08:58 +0800

    I had more free time so I updated the embed I made

--- v13-wok/commands/embed.ts
+++ v13-wok/commands/embed.ts
@@ -12,6 +12,12 @@ export default {
         const embed = new MessageEmbed()
         .setTitle('hello world')
         .setDescription('hello world')
+        .setColor('#FFFB00')
+        .addField('This is a field', 'This is a value')
+        .addField('This is what I do when I\'m bored', 'because I have nothing else to do')
+        .addField('I like programming', 'just like how you like looking at StackOverflow')
+        .setFooter('this is a feeter')
+        .setAuthor('TCA is good programmer', 'https://i.imgur.com/EmPoWzR.jpg')
 
         return embed
     }
